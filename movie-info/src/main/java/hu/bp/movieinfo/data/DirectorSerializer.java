8566e0dfb87281ee6e811a070cd8057f4f7f356d
Peter Borkuti <peterborkuti@gmail.com>
2019-02-10 14:54:32 +0100

    SF

--- movie-info/src/main/java/hu/bp/movieinfo/data/DirectorSerializer.java
+++ movie-info/src/main/java/hu/bp/movieinfo/data/DirectorSerializer.java
@@ -9,6 +9,15 @@ import java.util.List;
 
 public class DirectorSerializer extends JsonSerializer<List<String>> {
 
+	/**
+	 * Serialize directors.
+	 * If directors has only one director, it should be a simple string
+	 * otherwise an array
+	 * @param strings
+	 * @param jsonGenerator
+	 * @param serializerProvider
+	 * @throws IOException
+	 */
 	@Override
 	public void serialize(List<String> strings, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
 		if (strings != null && strings.size() == 1) {
